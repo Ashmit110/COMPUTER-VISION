80f50073b1a1d0106fe692e1dc208755cedffffa
Ashmit110 <89088826+Ashmit110@users.noreply.github.com>
2024-02-22 23:45:41 +0530

    update latent space sampling

--- CV.py
+++ CV.py
@@ -223,62 +223,62 @@ load_checkpoint(torch.load("C:\python learning\SAIDL\COMPUTER VISION\checkpoint_
 
 
 
-num_epochs = 60
-diz_loss = {'train_loss':[],'val_loss':[]}
-for epoch in range(num_epochs):
-   train_loss =train_epoch(encoder,decoder,device,
-   train_loader,loss_fn,optim)
-   val_loss = test_epoch(encoder,decoder,device,valid_loader,loss_fn)
-   print('\n EPOCH {}/{} \t train loss {} \t val loss {}'.format(epoch + 1, num_epochs,train_loss,val_loss))
-   diz_loss['train_loss'].append(train_loss)
-   diz_loss['val_loss'].append(val_loss)
+# num_epochs = 0
+# diz_loss = {'train_loss':[],'val_loss':[]}
+# for epoch in range(num_epochs):
+#    train_loss =train_epoch(encoder,decoder,device,
+#    train_loader,loss_fn,optim)
+#    val_loss = test_epoch(encoder,decoder,device,valid_loader,loss_fn)
+#    print('\n EPOCH {}/{} \t train loss {} \t val loss {}'.format(epoch + 1, num_epochs,train_loss,val_loss))
+#    diz_loss['train_loss'].append(train_loss)
+#    diz_loss['val_loss'].append(val_loss)
    
 
 
 
-encoded_samples = []
-from tqdm import tqdm
-for sample in tqdm(test_dataset):
-    img = sample[0].unsqueeze(0).to(device)
-    label = sample[1]
-    # Encode image
-    encoder.eval()
-    with torch.no_grad():
-        encoded_img  = encoder(img)
-    # Append to list
-    encoded_img = encoded_img[0].flatten().cpu().numpy()
-    encoded_sample = {f"Enc. Variable {i}": enc for i, enc in enumerate(encoded_img)}
-    encoded_sample['label'] = label
-    encoded_samples.append(encoded_sample)
-encoded_samples = pd.DataFrame(encoded_samples)
-encoded_samples
+# encoded_samples = []
+# from tqdm import tqdm
+# for sample in tqdm(test_dataset):
+#     img = sample[0].unsqueeze(0).to(device)
+#     label = sample[1]
+#     # Encode image
+#     encoder.eval()
+#     with torch.no_grad():
+#         encoded_img  = encoder(img)
+#     # Append to list
+#     encoded_img = encoded_img[0].flatten().cpu().numpy()
+#     encoded_sample = {f"Enc. Variable {i}": enc for i, enc in enumerate(encoded_img)}
+#     encoded_sample['label'] = label
+#     encoded_samples.append(encoded_sample)
+# encoded_samples = pd.DataFrame(encoded_samples)
+# encoded_samples
 
-from sklearn.manifold import TSNE
-import plotly.express as px
+# from sklearn.manifold import TSNE
+# import plotly.express as px
 
-tsne = TSNE(n_components=2)
-tsne_results = tsne.fit_transform(encoded_samples.drop(['label'],axis=1))
-fig = px.scatter(tsne_results, x=0, y=1,
-                  color=encoded_samples.label.astype(str),
-                    labels={'0': 'tsne-2d-one', '1': 'tsne-2d-two'})
-fig.show()
+# tsne = TSNE(n_components=2)
+# tsne_results = tsne.fit_transform(encoded_samples.drop(['label'],axis=1))
+# fig = px.scatter(tsne_results, x=0, y=1,
+#                   color=encoded_samples.label.astype(str),
+#                     labels={'0': 'tsne-2d-one', '1': 'tsne-2d-two'})
+# fig.show()
 
-checkpoint_encoder={
-               "state_dict":encoder.state_dict(),
-               "optimizer": optim.state_dict(),
-           }
-save_checkpoint(checkpoint_encoder,"C:\python learning\SAIDL\COMPUTER VISION\checkpoint_encoder")
+# checkpoint_encoder={
+#                "state_dict":encoder.state_dict(),
+#                "optimizer": optim.state_dict(),
+#            }
+# save_checkpoint(checkpoint_encoder,"C:\python learning\SAIDL\COMPUTER VISION\checkpoint_encoder")
 
-checkpoint_decoder={
-               "state_dict":decoder.state_dict(),
-               "optimizer": optim.state_dict(),
-           }
-save_checkpoint(checkpoint_decoder,"C:\python learning\SAIDL\COMPUTER VISION\checkpoint_decoder")
+# checkpoint_decoder={
+#                "state_dict":decoder.state_dict(),
+#                "optimizer": optim.state_dict(),
+#            }
+# save_checkpoint(checkpoint_decoder,"C:\python learning\SAIDL\COMPUTER VISION\checkpoint_decoder")
 
 
-plot_ae_outputs(encoder,decoder,n=10)
+# plot_ae_outputs(encoder,decoder,n=10)
 
-def latent_space_sampling(type,encoder,decoder):
+def latent_space_sampling(distribution,encoder,decoder):
     
     def show_image(img):
         npimg = img.numpy()
@@ -287,27 +287,41 @@ def latent_space_sampling(type,encoder,decoder):
     encoder.eval()
     decoder.eval()
 
-    with torch.no_grad():
+    if distribution=="normal":
         # calculate mean and std of latent code, generated takining in test images as inputs 
         for images, labels in test_loader:
             images = images.to(device)
             mu,sigma = encoder(images)
             
+            
 
             
 
             # sample latent vectors from the normal distribution
-            latent = torch.randn_like(sigma)*sigma + mu
+            latent = torch.rand_like(mu)
+            break
+    elif distribution=="gauss":
 
-            # reconstruct images from the random latent vectors
-            
-            img_recon = decoder(latent)
-            img_recon = img_recon.cpu()
-            
+        for images, labels in test_loader:
+            images = images.to(device)
+            mu,sigma = encoder(images)
 
-            fig, ax = plt.subplots(figsize=(20, 8.5))
-            show_image(torchvision.utils.make_grid(img_recon[:100],10,5))
-            plt.show()
+            # sample latent vectors from the normal distribution
+            latent = 1+(2)*torch.rand_like(mu)
             break
 
-latent_space_sampling("test",encoder,decoder)
+            
+
+
+    # reconstruct images from the random latent vectors
+            
+    img_recon = decoder(latent)
+    img_recon = img_recon.cpu()
+    
+
+    fig, ax = plt.subplots(figsize=(20, 8.5))
+    show_image(torchvision.utils.make_grid(img_recon[:100],10,5))
+    plt.show()
+            
+
+latent_space_sampling("gauss",encoder,decoder)
